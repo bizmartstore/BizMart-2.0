4b52133fd096b6513f008bae27c1e36b28e7e0dc
[dyad] <sheethappenswithjaa@gmail.com>
2026-04-05 16:56:33 +0800

    [dyad] Fixed import path for GenerateDescriptionButton in ProductForm component - wrote 1 file(s)

--- src/pages/ProductForm.tsx
+++ src/pages/ProductForm.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import GenerateDescriptionButton from "./GenerateDescriptionButton";
+import GenerateDescriptionButton from "../components/GenerateDescriptionButton";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
