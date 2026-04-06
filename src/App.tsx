86f4e9e2d5bedc6c6f1b0304eb86a26465d4c9da
[dyad] <sheethappenswithjaa@gmail.com>
2026-04-06 10:15:08 +0800

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
-<Routes>
-  <Route path="/admin*" element={<AdminDashboard />} />
-  <Route path="*" element={<NotFound />} />
-</Routes>
+import { Routes, Route } from "react-router-dom";
+import AdminDashboard from "./pages/AdminDashboard";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/admin*" element={<AdminDashboard />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
